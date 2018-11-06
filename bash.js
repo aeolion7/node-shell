858afa12b2033ce2434f44a1eac37f685dc99c05
Joseph Lionarons <lionsamurai@gmail.com>
2018-11-06 14:29:35 -0500

    implemented cat

--- bash.js
+++ bash.js
@@ -4,6 +4,7 @@ process.stdout.write('prompt > ');
 // The stdin 'data' event fires after a user types in a line
 const pwd = require('./pwd');
 const ls = require('./ls');
+const cat = require('./cat');
 
 process.stdin.on('data', (data) => { // event listener
   const cmd = data.toString().trim(); // remove new line
@@ -12,6 +13,9 @@ process.stdin.on('data', (data) => { // event listener
     pwd();
   } else if (cmd === 'ls') {
     ls();
+  } else if (cmd.startsWith('cat')) {
+    const fileName = cmd.split(' ')[1];
+    cat(fileName);
   }
 
   process.stdout.write('\nprompt > ');
--- cat.js
+++ cat.js
@@ -1,5 +1,8 @@
 const fs = require('fs');
 
-module.exports = function () {
-
-}
+module.exports = function (fileName) {
+  fs.readFile(`./${fileName}`, 'utf8', (err, contents) => {
+    if (err) throw err;
+    console.log(contents);
+  });
+};
